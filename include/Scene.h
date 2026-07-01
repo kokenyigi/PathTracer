680afca6df9a3c5d256de7fdc3004d965317ccbb
Ignác Kökény <f0wizz@INF.ELTE.HU>
2026-07-01 20:51:08 +0200

    Began developing Mesh Loader

--- include/Scene.h
+++ include/Scene.h
@@ -35,6 +35,13 @@ static glm::vec4 ClToGlm(const cl_float4& cl)
     return retval;
 }
 
+ 
+
+
+/**
+ * This structure is basically written into GPU memory each frame,
+ * Containes necessary information related to the user's camera.
+ */
 struct CameraData
 {
     cl_float4 position;
@@ -46,6 +53,62 @@ struct CameraData
     float zNear;
     float zFar;
 };
+
+/**
+ * Three - dimensional Axis Alligned Bounding Box
+ *  THe OpenCL way -> cl_float3 = cl_float4 allignment wise.
+ */
+struct AABB3
+{
+	cl_float4 min;
+    cl_float4 max;
+};
+
+/**
+ * Just a simple way to unify the namespace namings.
+ */
+typedef cl_float4 VertexPositionData;
+
+/**
+ * x,y and z store the 3 indices of the triangle.
+ */
+typedef cl_int4 TriangleIndicesData;
+
+/**
+ * In the Pathtraced sense its worth it to seperate vertex positions from other attributes.
+ * The reason is that the positions are accessed frequently, to check for arithmetic triangle-ray intersections
+ * However, the other attributes arent needed for this, therefore we get a bit of cache-optimization.
+ */
+struct VertexAttributeData
+{
+    cl_float4 normal;
+    cl_float2 textureCoords;
+};
+
+/**
+ * This structure stores the necessary informations for a BVH node.
+ * startindex == first triangle of the given box's triangle interval, endIndex == last
+ */
+struct BvhNodeData
+{
+    int startIndex = -1;
+    int endIndex = -1;
+    int maxChild = -1;
+    int minChild = -1;
+    AABB3 box;
+};
+
+/**
+ * This structure is just a way to enable communication of Mesh information to the outside eg.: View (App) layer
+ */
+struct MeshInfo
+{
+    int vertexCount = 0;
+    int triangleCount = 0;
+    int bvhNodeCount = 0;
+    int bvhDepth = 0;
+};
+
 
 /**
  * This class is basically a renderer, it handles IO, and renders the given scene into a texture.
@@ -81,7 +144,6 @@ private:
     Shader testShader;
     
 
-
     //OpenCL related variables
     cl_platform_id clPlatform;
     cl_device_id clDevice;
@@ -108,6 +170,14 @@ public:
     void MouseWheel(float amount, int direction);
     void KeyInput(int key, int action, int mods);
 
+    /**
+     * This functions tries its best to load a .obj mesh file into both the pathtraced and raytraced scene.
+     * Returns true if successful, false otherwise.
+     * With its second arg, one can querry information about the loaded mesh e.g.: bvh node count,
+     *  bvh depth, vertex count, triag count
+     */
+    bool TryLoadMesh(const std::string& filePathRelative, MeshInfo* meshInfo = nullptr);
+
 
     void Delete();
 
@@ -118,6 +188,14 @@ private:
     void RasterizeRender();
 
     void PathTracedRender();
+
+
+    bool TryLoadPathTracedMesh(const std::string& filePathRelative,
+        std::vector<VertexPositionData>& newMeshVertexPositions,
+        std::vector<TriangleIndicesData>& newMeshTriangleIndices,
+        std::vector<VertexAttributeData>& newMeshVertexAttributes,
+        MeshInfo* meshInfo);
+    
 
 };
 
